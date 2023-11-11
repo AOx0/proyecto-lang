97e333e416b5845b1b100f984cc7003de32ce22e
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-11-10 21:50:22 -0600

    Add docstrings

--- src/vector.h
+++ src/vector.h
@@ -1,3 +1,11 @@
+//! Declaración de un vector en C que 
+//! puede almacenar $N$ elementos de un 
+//! tamaño genérico de bytes.
+//!
+//! El vector almacena el numero de bytes
+//! que ocupa el tipo de dato para poder
+//! realizar operaciones como `push`, `pop`.
+
 #ifndef LNG_VECTOR
 #define LNG_VECTOR
 
@@ -6,6 +14,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+/// Vec
+///
+/// La estructura Vec sabe la dirección de de
+/// memoria de la que es dueña, y el numero de
+/// elementos de tamaño `t_size` que puede 
+/// almacenar y que almacena.
 struct Vec {
         uint8_t *ptr;
         size_t cap;
@@ -14,6 +28,12 @@ struct Vec {
 };
 typedef struct Vec Vec;
 
+/// VecSlice
+///
+/// Como en otros lenguajes de programación, se
+/// trata de una estructura que tiene acceso
+/// (solo de lectura) a una fracción de los 
+/// bytes de un arreglo o `Vec`.
 struct VecSlice {
         uint8_t *ptr;
         size_t len;
@@ -21,13 +41,79 @@ struct VecSlice {
 };
 typedef struct VecSlice VecSlice;
 
+/// vec_new
+/// 
+/// Crear un nuevo vector que almacenará
+/// valores que tienen un tamaño de 
+/// bytes `t_size`. 
+///
+/// Los vectores no reservan memoria a menos que
+/// se haga `push` de un valor(es). Para crear
+/// un vector que reserva una cabtidad
+/// determinada de bytes durante su creación
+/// usar `vec_with_cap`, 
+/// `vec_init_with_cap`, etc.
 Vec vec_new(size_t t_size);
+
+/// vec_grow
+///
+/// Asegura que el vector tiene espacio para 
+/// almacenar `cap` elementos adicionales.
+/// Reserva mas memoria con `realloc` en caso
+/// de haber agotado el espacio.
 void vec_grow(Vec *v, size_t cap);
+
+/// vec_with_cap
+///
+/// Crea un vector que reserva la cantidad de memoria necesaria para almacenar `cap` 
+/// número de elementos
 Vec vec_with_cap(size_t t_size, size_t cap);
+
+/// vec_shrink
+///
+/// Libera la memoria extra de un vector de
+/// forma que su capacidad de almacenamiento
+/// sea la misma que el numero se elementos
+/// que contiene.
 void vec_shrink(Vec *v);
+
+/// vec_init
+///
+/// Inicializa una instancia de vector que
+/// puede almacenar elementos de `t_size` 
+/// bytes de tamaño en el apuntador indicado.
 void vec_init(Vec *v, size_t t_size);
+
+/// vec_int_with_cap
+///
+/// Inicializa un vector directamente en la
+/// dirección de memoria indicada con capacidad
+/// reservada con malloc para retener `cap` 
+/// numero de elementos.
 void vec_init_with_cap(Vec *v, size_t t_size, size_t cap);
+
+///vec_get
+///
+/// Devuelve un apuntador al elemento $i$
+/// almacenado en el vector.
+///
+/// *Panics*
+///
+/// Termina la  ejecución del programa con
+/// un mensaje de  error en caso de que el
+/// indice no sea valido.
 void *vec_get(Vec *v, size_t idx);
+
+/// vec_push
+///
+/// Devuelve un apuntador a la cima de la pila
+/// de forma que el usuario pueda _castear_ la
+/// dirección y escribir en ella el elemento en
+/// sí.
+///
+/// Si el vector no tiene la capacidad de
+/// contener un nuevo elemento, entonces reserva
+/// espacio asicional para 1 elemwnto.
 void *vec_push(Vec *v);
 void vec_remove(Vec *v, size_t idx);
 void *vec_pop(Vec *v);
